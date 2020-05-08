0b6266e38952026ebca8203c4bc8e2d71d127880
Arnab <arnabd2006@gmail.com>
2020-05-08 12:57:32 +0200

    Master Commit

--- src/test/java/com/example/springdockerdemo/SpringDockerDemoApplicationTests.java
+++ src/test/java/com/example/springdockerdemo/SpringDockerDemoApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringDockerDemoApplicationTests {
 
+	//the line is in the master branch
 	@Test
 	void contextLoads() {
 	}
