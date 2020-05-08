60ab7068b77e3c90ce69c5bf4ceb03488480684f
Arnab <arnabd2006@gmail.com>
2020-05-08 12:56:15 +0200

    Demo commit

--- src/test/java/com/example/springdockerdemo/SpringDockerDemoApplicationTests.java
+++ src/test/java/com/example/springdockerdemo/SpringDockerDemoApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringDockerDemoApplicationTests {
+	//I have adde a line here which is not in Matser
 
 	@Test
 	void contextLoads() {
