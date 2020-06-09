ce845ed1b0aac07521e31d2a50d4c0509e53889f
Arnab <arnabd2006@gmail.com>
2020-06-09 23:20:25 +0200

    Demo Commit

--- src/main/java/com/example/springdockerdemo/controller/AdapterFacade.java
+++ src/main/java/com/example/springdockerdemo/controller/AdapterFacade.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AdapterFacade{
-    @RequestMapping("/")
+    @RequestMapping("/demoTest")
     public String something(){
         System.out.println("Getting hits");
         return "Hey You!";
